86c599c7ccbfdccbb33e2a04d14f506b98358f01
Chloe Yeo <yeochloe01@gmail.com>
2024-04-16 14:49:51 +0900

    first commit - decrement implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Hi <ChildOne />
+    </h1>
+  );
+}
+
+function ChildOne() {
+  const dispatch = useDispatch();
+  const num = useSelector((state) => {
+    return state.num;
+  });
+  const decrement = () => {
+    dispatch({ type: "decrement", payload: "2" });
+  };
+  return (
+    <>
+      <h1>child one num: {num}</h1>
+      <button onClick={decrement}>decrement</button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createStore } from "redux"; // 1.
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+// storage defaults to localstorage for web applications.
+import storage from "redux-persist/lib/storage"; // identify which storage to use
+import { persistStore, persistReducer } from "redux-persist";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/* Redux:
+1. Create a store
+2. Make a reducer (to put it in create store param)
+3. Create a provider to surround app in. Pass in store into provider
+*/
+
+const initialState = {
+  num: 900,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "decrement":
+      return {
+        ...state,
+        num: state.num - action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// persistConfig defines the configuration for persistence
+const persistConfig = {
+  key: "root", // the key under which the persisted state will be stored
+  // key can be named anything other than root also
+  storage, // default to localstorage
+};
+
+// persistReducer takes a regular Redux reducer and returns a new
+// reducer with state persistence capabilities (i.e. state gets stored in localstorage
+// and does not disappear when user reloads the page or shuts down page)
+const myPersistReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(myPersistReducer);
+
+// persistStore actually stores data in localstorage (so must add this line).
+// persistStore function from redux-persist takes your Redux store and
+// returns a persisted version of it.
+// it creates a Redux store that automatically reads from and writes to
+// the specified storage mechanism (localStorage) based on the provided configuration
+// (persistConfig). It enhances the Redux store created by createStore with
+// persistence capabilities, allowing your application's state to persist "across sessions".
+const persistor = persistStore(store);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
